4116483542749272fcab3c729710b8ff079617ed
LastRussianWinter <120733989+LastRussianWinter@users.noreply.github.com>
2023-04-10 07:25:39 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,9 @@
+local repo = "https://raw.githubusercontent.com/LastRussianWinter/ByteAI/main/"
 
+local gameID = game.GameId
+
+local tbl = {}
+
+if tbl[gameID] then
+    loadstring(game:HttpGet(repo..tostring(gameID)))()
+end
